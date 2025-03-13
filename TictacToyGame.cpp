64c296287c6ed767c6fa2c1954ab0e04577f8d68
Sana Afzal <sanaafzalsana123@gmail.com>
2025-03-13 09:17:39 -0700

    Add files via upload

---
+++ TictacToyGame.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+int main(){
+cout<<".  Tic Tac toy game\n";
+cout<<"\tPlayer1 [x]\n\tPlayer2 [o]\n\n";
+
+cout<<"\t\t   |   |   \n";
+cout<<"\t\t 1 | 2 | 3 \n";
+cout<<"\t\t___________\n";
+cout<<"\t\t   |   |   \n";
+cout<<"\t\t 4 | 5 |6  \n";
+cout<<"\t\t___________\n ";
+cout<<"\t\t   |   |   \n";
+cout<<"\t\t 7 | 8 | 9 \n";
+cout<<" Player 2 [x] turn on:";
+}
